9964cbdd35b3f2900ebc8ca106c07356305aceaa
Rayudo11 <bryanmendez209@gmail.com>
2022-04-05 23:09:58 -0500

    Last Commit

--- sql/connection.js
+++ sql/connection.js
@@ -1,15 +1,23 @@
 const mysql = require('mysql')
 
+require('dotenv').config();
+
+const {DB_HOST,DB_USER,DB_PASSWORD,DB_DATABASE} = process.env
+
 class Connection {
   constructor() {
     if (!this.pool) {
       console.log('creating connection...')
       this.pool = mysql.createPool({
         connectionLimit: 100,
-        host: 'localhost',
-        user: 'root',
-        password: 'password',
-        database: 'admin'
+        // host: DB_HOST,
+        // user: DB_USER,
+        // password: DB_PASSWORD,
+        // database: DB_DATABASE
+        host: process.env.DB_HOST,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_DATABASE
       })
 
       return this.pool
